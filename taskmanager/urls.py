8a2d9720e24c12367c516570ca98f97fad0158c6
dorovka <127792369+dorovka@users.noreply.github.com>
2023-03-15 15:53:52 +0300

    Update urls.py

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path("favicon.ico", RedirectView.as_view(url=static("favicon.ico"))). (django.templatetags.static),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
