63ecf19b520354be33eacaa42790d805c0ae7338
dorovka <127792369+dorovka@users.noreply.github.com>
2023-03-15 16:01:51 +0300

    Update wsgi.py

--- taskmanager/wsgi.py
+++ taskmanager/wsgi.py
@@ -3,6 +3,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = '/home/test/taskmanager/taskmanager/mysite'
+if path not in sys.path:
+sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
 
 application = get_wsgi_application()
